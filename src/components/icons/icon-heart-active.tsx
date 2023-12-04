98b44ad2858401987781b27d7a3560ef4e731bb2
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-04 17:24:00 -0300

    create component icons

---
+++ src/components/icons/icon-heart-active.tsx
@@ -0,0 +1,15 @@
+export const IconHeartActive = ({ className }: { className: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 20 20"
+      className={className}
+    >
+      <path
+        fill="#E07B67"
+        d="M18.75 7.344c0 5.468-8.109 9.895-8.454 10.078a.626.626 0 01-.592 0c-.345-.183-8.454-4.61-8.454-10.078A4.85 4.85 0 016.094 2.5c1.613 0 3.026.694 3.906 1.866.88-1.172 2.293-1.866 3.906-1.866a4.85 4.85 0 014.844 4.844z"
+      ></path>
+    </svg>
+  )
+}
---
+++ src/components/icons/icon-search.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const IconSearch = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 18 18"
+      className={className}
+    >
+      <path
+        fill="#E07B67"
+        d="M16.148 15.352l-3.52-3.52a6.195 6.195 0 10-.796.795l3.52 3.521a.564.564 0 00.96-.398.563.563 0 00-.164-.398zM2.812 7.875a5.063 5.063 0 1110.126 0 5.063 5.063 0 01-10.126 0z"
+      ></path>
+    </svg>
+  )
+}
