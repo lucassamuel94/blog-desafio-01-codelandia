f6bf7c78db3f842f7c3348c06647672b77c2d15a
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-04 17:24:39 -0300

    include component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { Header } from "./components/header";
 
 export default function App() {
   return (
     <>
-      <h1>Blog</h1>
-      <p>Desafio 01 - Codelândia</p>
+      <Header />
     </>
   )
 }
